0e93de10564b71afb2d01cd706c5dab1bcc6b0a9
Sanju <swamisanjoo@gmail.com>
2024-01-19 21:46:53 +0530

    revision start

---
+++ app/src/main/java/com/example/applicationforblackcoffer/MainActivity2.java
@@ -0,0 +1,18 @@
+package com.example.applicationforblackcoffer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.applicationforblackcoffer.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    ActivityMain2Binding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
